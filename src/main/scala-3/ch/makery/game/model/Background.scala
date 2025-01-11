9474fe97206d3914f36f1615af6a96f28abb8bd5
atikvh <153811795+atikvh@users.noreply.github.com>
2025-01-11 21:03:14 +0800

    Background draft

---
+++ src/main/scala-3/ch/makery/game/model/Background.scala
@@ -0,0 +1,10 @@
+// 22100259 Final Project Assignment
+package ch.makery.game.model
+
+import scalafx.scene.image.Image
+
+class Background(level: GameLevel) {
+  //val image: Image = new Image(getClass.getResource(level.backgroundImagePath).toExternalForm)
+  
+  //def getImage: Image = image
+}
