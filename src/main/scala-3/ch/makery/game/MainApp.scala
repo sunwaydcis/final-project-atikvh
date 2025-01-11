24e548a428156d55b8ec4e5c4b6596c863d65d86
atikvh <153811795+atikvh@users.noreply.github.com>
2025-01-12 07:48:23 +0800

    MainApp

---
+++ src/main/scala-3/ch/makery/game/MainApp.scala
@@ -0,0 +1,17 @@
+package ch.makery.game
+
+import scalafx.application.JFXApp3
+import scalafx.scene.Scene
+import javafx.fxml.FXMLLoader
+import scalafx.scene as sfxs
+import javafx.scene as jfxs
+import scalafx.application.JFXApp3
+import scalafx.application.JFXApp3.PrimaryStage
+import scalafx.Includes.*
+
+object MainApp extends JFXApp3 {
+  var roots: Option[sfxs.layout.BorderPane] = None
+  override def start(): Unit = {
+    val rootResource = getClass.getResource("view/RootLayout.fxml")
+  }
+}
