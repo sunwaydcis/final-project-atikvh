9756d05e5daa501eb57acce86fe3de29bcce8dc4
atikvh <153811795+atikvh@users.noreply.github.com>
2025-01-11 21:08:29 +0800

    Background with game level

--- src/main/scala-3/ch/makery/game/model/Background.scala
+++ src/main/scala-3/ch/makery/game/model/Background.scala
@@ -4,7 +4,7 @@ package ch.makery.game.model
 import scalafx.scene.image.Image
 
 class Background(level: GameLevel) {
-  //val image: Image = new Image(getClass.getResource(level.backgroundImagePath).toExternalForm)
+  val image: Image = new Image(getClass.getResource(level.backgroundImagePath).toExternalForm)
   
-  //def getImage: Image = image
+  def getImage: Image = image
 }
--- src/main/scala-3/ch/makery/game/model/GameLevel.scala
+++ src/main/scala-3/ch/makery/game/model/GameLevel.scala
@@ -1,14 +1,19 @@
 // 22100259 Final Project Assignment
 package ch.makery.game.model
 
-abstract class GameLevel(val name: String, val initialTimer: Int, val characterProbability: Map[String, Int])
+abstract class GameLevel(
+                          val name: String,
+                          val initialTimer: Int,
+                          val characterProbability: Map[String, Int],
+                          val backgroundImagePath: String)
 
 case class EasyLevel() extends GameLevel(
   name = "Easy",
   initialTimer = 180,
   characterProbability = Map(
     "Brown Mole" -> 100
-  )
+  ),
+  backgroundImagePath = "/ch/makery/game/assets/Easy.png"
 )
 
 case class MediumLevel() extends GameLevel(
@@ -18,7 +23,8 @@ case class MediumLevel() extends GameLevel(
     "Brown Mole" -> 70,
     "Grey Mole" -> 20,
     "Bomb" -> 10
-  )
+  ),
+  backgroundImagePath = "/ch/makery/game/assets/Medium.png"
 )
 
 case class HardLevel() extends GameLevel(
@@ -29,5 +35,6 @@ case class HardLevel() extends GameLevel(
     "Pink Mole" -> 25,
     "Grey Mole" -> 15,
     "Bomb" -> 10
-  )
+  ),
+  backgroundImagePath = "/ch/makery/game/assets/Hard.png"
 )
