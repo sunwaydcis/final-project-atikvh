e38d1e42a77abc945361f170f25af62d6eea89c6
atikvh <153811795+atikvh@users.noreply.github.com>
2025-01-15 05:24:48 +0800

    adding loop

--- src/main/scala-3/ch/makery/game/model/Game.scala
+++ src/main/scala-3/ch/makery/game/model/Game.scala
@@ -15,12 +15,14 @@ class Game {
   def timer: Int = _timer
   def level: GameLevel = _level
   def history: Seq[GameHistory] = _history.toSeq // Immutable history view
-  
+  def speed: Int = _speed
 
   def startGame(level: GameLevel): Unit = {
     _level = level
     _score = 0
     _timer = _level.initialTimer
+    _speed = _level.speed
+    startGameLoop()
   }
 
   def updateScore(points: Int): Unit = {
@@ -48,6 +50,37 @@ class Game {
   def reset(): Unit = {
     _score = 0
     _timer = _level.initialTimer
+  }
+  
+  private def startGameLoop(): Unit = {
+    new Thread(()=> {
+      while (!isGameOver) {
+        spawnCharacter()
+        Thread.sleep(_speed)
+      }
+    }).start()
+    new Thread(()=> {
+      while(!isGameOver) {
+        Thread.sleep(1000)
+        decrementTimer()
+      }
+    }).start()
+  }
+  
+  private def spawnCharacter(): Unit = {
+    val character = generateRandomCharacter()
+  }
+  
+  private def generateRandomCharacter(): Unit = {
+    val randomValue = scala.util.Random.nextInt(100)
+    _level.characterProbability.collectFirst {
+      case (character, threshold) if randomValue < threshold => character match {
+        case "Brown Mole" => new BrownMole()
+        case "Pink Mole" => new PinkMole()
+        case "Grey Mole" => new GreyMole()
+        case "Bomb" => new Bomb()
+      }
+    }.getOrElse(new BrownMole())
   }
 }
 
